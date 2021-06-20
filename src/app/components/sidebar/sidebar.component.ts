b3f54f443cbd4feb4fcd0e36541448b388aa64ba
Kais Rouini <kais.rouini@gmail.com>
2021-06-20 16:41:01 +0100

    fixed typo

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,7 +10,7 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: '/user-profile', title: 'Users',  icon:'person', class: '' },
-    { path: '/table-list', title: 'Resataurants List',  icon:'content_paste', class: '' }
+    { path: '/table-list', title: 'Restaurants List',  icon:'content_paste', class: '' }
     //{ path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
     //{ path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
     //{ path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
